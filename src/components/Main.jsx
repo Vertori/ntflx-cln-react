ff4fb9e0c8656b1e5ab80a52029b3bd5984c58eb
Vertori <115083330+Vertori@users.noreply.github.com>
2023-05-08 17:27:33 +0200

    API request end points, fetching data

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import requests from "../Request";
+import axios from "axios";
+
+const Main = () => {
+  const [movies, setMovies] = useState([]);
+
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
+
+  return <div>Main</div>;
+};
+
+export default Main;
