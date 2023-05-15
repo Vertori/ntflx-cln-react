525a1b76cada6b3f693af2ecaa3634a5d4c54ea5
Vertori <115083330+Vertori@users.noreply.github.com>
2023-05-15 15:07:31 +0200

    sign up component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import netflixLogo from "../assets/netflix-logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="w-full flex items-center justify-between py-6 px-12 z-[100] fixed">
-      <img className="w-32" src={netflixLogo} />
+      <Link to="/">
+        <img className="w-32" src={netflixLogo} />
+      </Link>
       <div>
-        <button className="text-white pr-4 cursor-pointer">Sign In</button>
-        <button className="bg-red-600 px-6 py-2 rounded-[8px] cursor-pointer text-white">
-          Sign Up
-        </button>
+        <Link to="/login">
+          <button className="text-white pr-4 cursor-pointer">Sign In</button>
+        </Link>
+        <Link to="/signup">
+          <button className="bg-red-600 px-6 py-2 rounded-[8px] cursor-pointer text-white">
+            Sign Up
+          </button>
+        </Link>
       </div>
     </div>
   );
