773fe19bf4f5bacb5c4dde0508f67206b5eb373c
Vertori <115083330+Vertori@users.noreply.github.com>
2023-05-18 11:13:01 +0200

    conditional navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import netflixLogo from "../assets/netflix-logo.png";
 import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const {user, logOut} = UserAuth()
+
+
   return (
     <div className="w-full flex items-center justify-between py-6 px-12 z-[100] fixed">
       <Link to="/">
         <img className="w-32" src={netflixLogo} />
       </Link>
-      <div>
+     {user?.email ?  <div>
+        <Link to="/account">
+          <button className="text-white pr-4 cursor-pointer">Account</button>
+        </Link>
+        <Link to="/signup">
+          <button className="bg-red-600 px-6 py-2 rounded-[8px] cursor-pointer text-white">
+            Logout
+          </button>
+        </Link>
+      </div> :  <div>
         <Link to="/login">
           <button className="text-white pr-4 cursor-pointer">Sign In</button>
         </Link>
@@ -17,7 +30,7 @@ const Navbar = () => {
             Sign Up
           </button>
         </Link>
-      </div>
+      </div>}
     </div>
   );
 };
