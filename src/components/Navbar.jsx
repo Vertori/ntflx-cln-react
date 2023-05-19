cba1c3019c36aa5abe5440d74b2889c2ed0a4542
Vertori <115083330+Vertori@users.noreply.github.com>
2023-05-19 16:44:18 +0200

    logout functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,51 @@
 import React from "react";
 import netflixLogo from "../assets/netflix-logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
-  const {user, logOut} = UserAuth()
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
 
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="w-full flex items-center justify-between py-6 px-12 z-[100] fixed">
       <Link to="/">
         <img className="w-32" src={netflixLogo} />
       </Link>
-     {user?.email ?  <div>
-        <Link to="/account">
-          <button className="text-white pr-4 cursor-pointer">Account</button>
-        </Link>
-        <Link to="/signup">
-          <button className="bg-red-600 px-6 py-2 rounded-[8px] cursor-pointer text-white">
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="text-white pr-4 cursor-pointer">Account</button>
+          </Link>
+
+          <button
+            onClick={handleLogout}
+            className="bg-red-600 px-6 py-2 rounded-[8px] cursor-pointer text-white"
+          >
             Logout
           </button>
-        </Link>
-      </div> :  <div>
-        <Link to="/login">
-          <button className="text-white pr-4 cursor-pointer">Sign In</button>
-        </Link>
-        <Link to="/signup">
-          <button className="bg-red-600 px-6 py-2 rounded-[8px] cursor-pointer text-white">
-            Sign Up
-          </button>
-        </Link>
-      </div>}
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="text-white pr-4 cursor-pointer">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded-[8px] cursor-pointer text-white">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
